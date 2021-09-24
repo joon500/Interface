a64eee0741f2d9a7d7f947528863525bb8d2f520
joon500 <maliar.kursk@gmail.com>
2021-09-24 15:43:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace My4
+{
+    public interface IMyList<T>
+    {
+        void Add2(T a);
+    }
+    class Program
+    {
+
+        public class MyList<T> : IMyList<T>
+        {
+            public int ff;
+            public void Add2(T a)
+            {
+                //add same
+            }
+        }
+        static void Main(string[] args)
+        {
+            IMyList<int> list1 = new MyList<int>(); // 1 вариант
+
+            var list2 = new MyList<int>(); // 2 вариант
+
+            MyList<int> list3 = new MyList<int>(); // 3 вариант
+
+
+
+
+        }
+    }
+}
